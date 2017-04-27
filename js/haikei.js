57e106fdbc09759358633d58175fced842ccc239
kenzkenzM <kenotiai@gmail.com>
2017-04-27 23:21:48 +0900

    修正

--- js/haikei.js
+++ js/haikei.js
@@ -2,7 +2,7 @@ $(function(){
     $("body").on("click",".haikei_Btn",function(){
         var id = "haikei_Dialog";
         var content = "";
-            content += "作成中作成中作成中作成中作成中作成中<br>作成中作成中作成中作成中作成中作成中";
+            content += "<table class='table table-bordered table-condensed'><tr><th>作成中</th><th>作成中作成中</th></tr><tr><td>作成中</td><td>作成中作成中</td></tr></table>";
         myDialog({
             id:id,
             map:"map1",
