286ee1954fdde5662d37f284703b435d2a83da7a
kenzkenzM <kenotiai@gmail.com>
2017-04-27 23:00:59 +0900

    ダイアログ生成コード追加

--- js/haikei.js
+++ js/haikei.js
@@ -1,6 +1,16 @@
 $(function(){
     $("body").on("click",".haikei_Btn",function(){
-        alert("作成中");
-    });
+        var id = "haikei_Dialog";
+        var content = "";
+            content += "作成中作成中作成中作成中作成中作成中<br>作成中作成中作成中作成中作成中作成中";
+        myDialog({
+            id:id,
+            map:"map1",
+            title:"背景",
+            content:content,
+            top:"50px",
+            right:"10px"
+        });
 
+    });
 });
