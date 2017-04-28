3c4221685c4d3fbbfd8f1810b1822e26bc7ff085
kenzkenzWS <kenotiai@gmail.com>
2017-04-28 12:49:43 +0900

    修正

--- js/startup.js
+++ js/startup.js
@@ -1,29 +1,18 @@
 var map1 = null;
 $(function(){
-    //------------------------------------------------------------------------------------------
+    //--------------------------------------------------------------------------
     //起動時に画面リサイズ
     $("#map1").height($(window).height());
     $(window).on('resize',function(){
         $("#map1").height($(window).height());
     });
-    //------------------------------------------------------------------------------------------
-    //国土地理院淡色地図のレイヤー
-    var pale = new ol.layer.Tile({
-        title:"国土地理院_単色地図",
-        type:"base",
-        source:new ol.source.XYZ({
-            attributions:[new ol.Attribution({html:"<a href='http://maps.gsi.go.jp/development/ichiran.html' target='_blank'>国土地理院</a>"})],
-            url:"http://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png",
-            minZoom:2,
-            maxZoom:18
-        })
-    })
-    //id mapにレイヤーをセット
+    //--------------------------------------------------------------------------
+    //id map1に起動時に表示されるレイヤーをセット
     map1 = new ol.Map({
         target:"map1",
-        layers:[pale],
+        layers:[pale],//raster.jsを参照。paleは地理院単色地図
         view:new ol.View({
-            center:ol.proj.fromLonLat([131.35190,31.62895]),//中心地を飫肥城にに
+            center:ol.proj.fromLonLat([131.35190,31.62895]),//中心地を飫肥城に
             zoom:16//ズーム率
         })
     });
