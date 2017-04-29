ef89da0c95e962c72e981dbffd371197d19602fa
kenzkenzM <kenotiai@gmail.com>
2017-04-29 22:45:48 +0900

    修正

--- js/haikei.js
+++ js/haikei.js
@@ -3,7 +3,6 @@ $(function(){
         if($("#mydialog-haikei-dialog").length==0){
             var id = "haikei-dialog";
             var content = "";
-                content += "<table class='table table-bordered table-condensed'><tr><th>作成中</th><th>作成中作成中</th></tr><tr><td>作成中</td><td>作成中作成中</td></tr></table>";
             mydialog({
                 id:id,
                 map:"map1",
