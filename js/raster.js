29b28819619766e640b46777adcbdc0a5c754daa
kenzkenzM <kenotiai@gmail.com>
2017-04-29 15:21:27 +0900

    レイヤー追加、削除機能を追加

--- js/raster.js
+++ js/raster.js
@@ -3,7 +3,6 @@ var pale = new ol.layer.Tile({
     title:"国土地理院_単色地図",
     origin:"国土地理院",
     detail:"国土地理院の単色地図",
-    type:"base",
     source:new ol.source.XYZ({
         attributions:[new ol.Attribution({html:"<a href='http://maps.gsi.go.jp/development/ichiran.html' target='_blank'>国土地理院</a>"})],
         url:"http://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png",
@@ -16,7 +15,6 @@ var seamlessphoto = new ol.layer.Tile({
     title:"国土地理院_空中写真",
     origin:"国土地理院",
     detail:"国土地理院の空中写真",
-    type:"base",
     source:new ol.source.XYZ({
         attributions:[new ol.Attribution({html:"<a href='http://maps.gsi.go.jp/development/ichiran.html' target='_blank'>国土地理院</a>"})],
         url:"http://cyberjapandata.gsi.go.jp/xyz/seamlessphoto/{z}/{x}/{y}.jpg",
@@ -24,27 +22,49 @@ var seamlessphoto = new ol.layer.Tile({
         maxZoom:18
     })
 })
+//オープンストリートマップ
+var osm = new ol.layer.Tile({
+    title:"OpenStreetMap",
+    origin:"OpenStreetMap Japan",
+    detail:"OpenStreetMapは、道路地図などの地理情報データを誰でも利用できるよう、フリーの地理情報データを作成することを目的としたプロジェクトです。",
+	source: new ol.source.OSM
+});
+var ort = new ol.layer.Tile({
+    title:"宮崎県航空写真",
+    origin:"宮崎県県土整備部砂防課",
+    detail:"砂防課が平成２５年度に撮影した航空写真をオルソ補正したもの",
+	source: new ol.source.OSM
+});
 //使用するレイヤーを設定
-var useLayersArr = [pale,seamlessphoto];
+var useLayersArr = [pale,seamlessphoto,osm];
 //------------------------------------------------------------------------------
 //背景ダイアログ用のテーブルを作成する。haikei.jsで使っている。
 function haikeiTableCreate(){
     var htmlChar = "<table class='table table-bordered table-condensed'>";
     for(var i = 0; i <useLayersArr.length; i++){
+        if(i==0){
+            var chkChar = "checked";
+        }else{
+            var chkChar = "";
+        };
         var prop = useLayersArr[i].getProperties();
         htmlChar += "<tr>";
-        htmlChar += "<td><label><input type='checkbox' name='haikei-check' value='" + i + "'>" + prop["title"]+ "</label></td>";
+        htmlChar += "<td><label><input type='checkbox' name='haikei-check' value='" + i + "'" + chkChar + ">" + prop["title"]+ "</label></td>";
         htmlChar += "<td>作成中</td>";
         htmlChar += "</tr>";
     };
     htmlChar += "</table>";
-    //console.log(htmlChar);
-    $(".dialog-content").html(htmlChar)
+    $(".dialog-content").html(htmlChar);
 };
 //------------------------------------------------------------------------------
 //背景ダイアログ用のテーブルを作成する。haikei.jsで使っている。
 $(function(){
     $("body").on("change","input:checkbox[name='haikei-check']",function(){
-        console.log($(this).val());
+        var layer = useLayersArr[Number($(this).val())];
+        if($(this).prop('checked')){
+            map1.addLayer(layer);
+        }else{
+            map1.removeLayer(layer);
+        };
     });
 });
