e21018f60a2453b2ac645d3e7cd3870d568ae2d7
kenzkenzWS <kenotiai@gmail.com>
2017-05-02 12:37:35 +0900

    ファイル名修正

--- js/layer-00.js
+++ js/layer-00.js
@@ -1,267 +1,149 @@
-//国土地理院淡色地図のレイヤー
-var pale1 = new ol.layer.Tile({
-    title:"国土地理院_単色地図",
-    origin:"国土地理院",
-    detail:"国土地理院の単色地図",
-    source:new ol.source.XYZ({
-        attributions:[new ol.Attribution({html:"<a href='http://maps.gsi.go.jp/development/ichiran.html' target='_blank'>国土地理院</a>"})],
-        url:"http://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png",
-        minZoom:2,
-        maxZoom:18
-    })
-})
-var pale2 = new ol.layer.Tile({
-    title:"国土地理院_単色地図",
-    origin:"国土地理院",
-    detail:"国土地理院の単色地図",
-    source:new ol.source.XYZ({
-        attributions:[new ol.Attribution({html:"<a href='http://maps.gsi.go.jp/development/ichiran.html' target='_blank'>国土地理院</a>"})],
-        url:"http://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png",
-        minZoom:2,
-        maxZoom:18
-    })
-})
-//空中写真のレイヤー
-var seamlessphoto1 = new ol.layer.Tile({
-    title:"国土地理院_空中写真",
-    origin:"国土地理院",
-    detail:"国土地理院の空中写真",
-    source:new ol.source.XYZ({
-        attributions:[new ol.Attribution({html:"<a href='http://maps.gsi.go.jp/development/ichiran.html' target='_blank'>国土地理院</a>"})],
-        url:"http://cyberjapandata.gsi.go.jp/xyz/seamlessphoto/{z}/{x}/{y}.jpg",
-        minZoom:2,
-        maxZoom:18
-    })
-})
-var seamlessphoto2 = new ol.layer.Tile({
-    title:"国土地理院_空中写真",
-    origin:"国土地理院",
-    detail:"国土地理院の空中写真",
-    source:new ol.source.XYZ({
-        attributions:[new ol.Attribution({html:"<a href='http://maps.gsi.go.jp/development/ichiran.html' target='_blank'>国土地理院</a>"})],
-        url:"http://cyberjapandata.gsi.go.jp/xyz/seamlessphoto/{z}/{x}/{y}.jpg",
-        minZoom:2,
-        maxZoom:18
-    })
-})
-//オープンストリートマップ
-var osm1 = new ol.layer.Tile({
-    title:"OpenStreetMap",
-    origin:"OpenStreetMap Japan",
-    detail:"OpenStreetMapは、道路地図などの地理情報データを誰でも利用できるよう、フリーの地理情報データを作成することを目的としたプロジェクトです。",
-	source: new ol.source.OSM
+var useLayersArr1 = null;
+var useLayersArr2 = null
+$(function(){
+    //使用するレイヤーを設定
+    useLayersArr1 = [pale1,seamlessphoto1,osm1,ort1,tunami1,sinsuisoutei1,kikenkeiryuu1,
+                    kyuukeisyakikenkasyo1,tisitu1,mierune1,ryuuiki1,ecoris1,muro1,csArr1];
+    useLayersArr2 = [pale2,seamlessphoto2,osm2,ort2,tunami2,sinsuisoutei2,kikenkeiryuu2,
+                    kyuukeisyakikenkasyo2,tisitu2,mierune2,ryuuiki2,ecoris2,muro2,csArr2];
 });
-var osm2 = new ol.layer.Tile({
-    title:"OpenStreetMap",
-    origin:"OpenStreetMap Japan",
-    detail:"OpenStreetMapは、道路地図などの地理情報データを誰でも利用できるよう、フリーの地理情報データを作成することを目的としたプロジェクトです。",
-	source: new ol.source.OSM
-});
-//宮崎県オルソ
-var ort1 = new ol.layer.Tile({
-    title:"宮崎県航空写真",
-    origin:"",
-    detail:"",
-	extent:transformE([130.66371,31.34280,131.88045,32.87815]),
-	source: new ol.source.XYZ({
-		url:'http://mtile.pref.miyazaki.lg.jp/tile/ort/{z}/{x}/{-y}.png',
-		minZoom :1,
-		maxZoom:19
-	})
-});
-var ort2 = new ol.layer.Tile({
-    title:"宮崎県航空写真",
-    origin:"",
-    detail:"",
-	extent:transformE([130.66371,31.34280,131.88045,32.87815]),
-	source: new ol.source.XYZ({
-		url:'http://mtile.pref.miyazaki.lg.jp/tile/ort/{z}/{x}/{-y}.png',
-		minZoom :1,
-		maxZoom:19
-	})
-});
-//ハザードマップ関係------------------------------------------------------------------------------------------------------------------------------------------------------
-//津波
-var tunami1 = new ol.layer.Tile({
-    title:"津波浸水想定区域",
-    origin:"",
-    detail:"",
-	source: new ol.source.XYZ({
-		attributions:[new ol.Attribution({html:"<a href='http://maps.gsi.go.jp/development/ichiran.html' target='_blank'>国土地理院</a>"})],
-		url:"http://disaportal.gsi.go.jp/hazardmap_data/raster/tsunamishinsui/{z}/{x}/{y}.png",
-		minZoom :1,
-		maxZoom:17
-
-	})
-});
-var tunami2 = new ol.layer.Tile({
-    title:"津波浸水想定区域",
-    origin:"",
-    detail:"",
-	source: new ol.source.XYZ({
-		attributions:[new ol.Attribution({html:"<a href='http://maps.gsi.go.jp/development/ichiran.html' target='_blank'>国土地理院</a>"})],
-		url:"http://disaportal.gsi.go.jp/hazardmap_data/raster/tsunamishinsui/{z}/{x}/{y}.png",
-		minZoom :1,
-		maxZoom:17
-	})
-});
-//浸水想定
-var sinsuisoutei1 = new ol.layer.Tile({
-    title:"洪水浸水想定",
-    origin:"",
-    detail:"",
-	source: new ol.source.XYZ({
-		attributions:[new ol.Attribution({html:"<a href='http://maps.gsi.go.jp/development/ichiran.html' target='_blank'>国土地理院</a>"})],
-		url:"http://disaportal.gsi.go.jp/hazardmap_data/raster/SHINSUI/{z}/{x}/{y}.png",
-		minZoom :1,
-		maxZoom:18
-	})
-});
-var sinsuisoutei2 = new ol.layer.Tile({
-    title:"洪水浸水想定区域",
-    origin:"",
-    detail:"",
-	source: new ol.source.XYZ({
-		attributions:[new ol.Attribution({html:"<a href='http://maps.gsi.go.jp/development/ichiran.html' target='_blank'>国土地理院</a>"})],
-		url:"http://disaportal.gsi.go.jp/hazardmap_data/raster/SHINSUI/{z}/{x}/{y}.png",
-		minZoom :1,
-		maxZoom:18
-	})
-});
-//土石流危険渓流
-var kikenkeiryuu1 = new ol.layer.Tile({
-    title:"土石流危険渓流",
-    origin:"",
-    detail:"",
-	source: new ol.source.XYZ({
-		attributions:[new ol.Attribution({html:"<a href='http://maps.gsi.go.jp/development/ichiran.html' target='_blank'>国土地理院</a>"})],
-		url:"http://disaportal.gsi.go.jp/hazardmap_data/raster/dosekiryukikenkeiryu/{z}/{x}/{y}.png",
-		minZoom :1,
-		maxZoom:16
-	})
-});
-var kikenkeiryuu2 = new ol.layer.Tile({
-    title:"土石流危険渓流",
-    origin:"",
-    detail:"",
-	source: new ol.source.XYZ({
-		attributions:[new ol.Attribution({html:"<a href='http://maps.gsi.go.jp/development/ichiran.html' target='_blank'>国土地理院</a>"})],
-		url:"http://disaportal.gsi.go.jp/hazardmap_data/raster/dosekiryukikenkeiryu/{z}/{x}/{y}.png",
-		minZoom :1,
-		maxZoom:16
-	})
-});
-//急傾斜地崩壊危険箇所
-var kyuukeisyakikenkasyo1 = new ol.layer.Tile({
-    title:"急傾斜地崩壊危険箇所",
-    origin:"",
-    detail:"",
-	source: new ol.source.XYZ({
-		attributions:[new ol.Attribution({html:"<a href='http://maps.gsi.go.jp/development/ichiran.html' target='_blank'>国土地理院</a>"})],
-		url:"http://disaportal.gsi.go.jp/hazardmap_data/raster/kyukeisyachihoukai/{z}/{x}/{y}.png",
-		minZoom :1,
-		maxZoom:16
-	})
-});
-var kyuukeisyakikenkasyo2 = new ol.layer.Tile({
-    title:"急傾斜地崩壊危険箇所",
-    origin:"",
-    detail:"",
-	source: new ol.source.XYZ({
-		attributions:[new ol.Attribution({html:"<a href='http://maps.gsi.go.jp/development/ichiran.html' target='_blank'>国土地理院</a>"})],
-		url:"http://disaportal.gsi.go.jp/hazardmap_data/raster/kyukeisyachihoukai/{z}/{x}/{y}.png",
-		minZoom :1,
-		maxZoom:16
-	})
-});
-//シームレス地質図
-var tisitu1 = new ol.layer.Tile({
-    title:"シームレス地質図",
-    origin:"",
-    detail:"",
-	source: new ol.source.XYZ({
-		url:"https://gbank.gsj.jp/seamless/tilemap/detailed/glfn/{z}/{y}/{x}.png",//ｘとｙを国土地理院流の反対にすること。
-		attributions:[new ol.Attribution({html:"<a href='https://www.gsj.jp/HomePageJP.html' target='_blank'>産業技術総合研究所地質調査総合センター</a>"})],
-		minZoom:5,
-		maxZoom:13
-	})
-});
-var tisitu2 = new ol.layer.Tile({
-    title:"シームレス地質図",
-    origin:"",
-    detail:"",
-	source: new ol.source.XYZ({
-		url:"https://gbank.gsj.jp/seamless/tilemap/detailed/glfn/{z}/{y}/{x}.png",//ｘとｙを国土地理院流の反対にすること。
-		attributions:[new ol.Attribution({html:"<a href='https://www.gsj.jp/HomePageJP.html' target='_blank'>産業技術総合研究所地質調査総合センター</a>"})],
-		minZoom:5,
-		maxZoom:13
-	})
-});
-//ミエルネ地図
-var mierune1 = new ol.layer.Tile({
-    title:"ミエルネ地図",
-    origin:"",
-    detail:"",
-	source: new ol.source.XYZ({
-		attributions:[new ol.Attribution({html:"Maptiles by <a href='http://www.mierune.co.jp/' target='_blank'><label>MIERUNE</label></a>, under CC BY. Data by <a href='http://osm.org/copyright' target='_blank'><label>OpenStreetMap</label></a> contributors, under ODbL."})],
-		url:"https://tile.mierune.co.jp/mierune/{z}/{x}/{y}.png",
-		maxZoom:15
-	})
-});
-var mierune2 = new ol.layer.Tile({
-    title:"ミエルネ地図",
-    origin:"",
-    detail:"",
-	source: new ol.source.XYZ({
-		attributions:[new ol.Attribution({html:"Maptiles by <a href='http://www.mierune.co.jp/' target='_blank'><label>MIERUNE</label></a>, under CC BY. Data by OpenStreetMap contributors, under ODbL."})],
-		url:"https://tile.mierune.co.jp/mierune/{z}/{x}/{y}.png",
-		maxZoom:15
-	})
-});
-//川と流域地図
-var ryuuiki1 = new ol.layer.Tile({
-    title:"川と流域地図",
-    origin:"",
-    detail:"",
-	source: new ol.source.XYZ({
-		attributions:[new ol.Attribution({html:"<a href='http://tiles.dammaps.jp/ryuiki/' target='_blank'>川と流域地図</a>"})],
-		//url:"./php/newproxy.php?url=http://tiles.dammaps.jp/ryuiki_t/1/{z}/{x}/{y}.png",
-        url:"http://tiles.dammaps.jp/ryuiki_t/1/{z}/{x}/{y}.png",
-		minZoom:4,
-		maxZoom:14
-	})
-});
-var ryuuiki2 = new ol.layer.Tile({
-    title:"川と流域地図",
-    origin:"",
-    detail:"",
-	source: new ol.source.XYZ({
-		attributions:[new ol.Attribution({html:"<a href='http://tiles.dammaps.jp/ryuiki/' target='_blank'>川と流域地図</a>"})],
-		url:"http://tiles.dammaps.jp/ryuiki_t/1/{z}/{x}/{y}.png",
-		minZoom:4,
-		maxZoom:14
-	})
-});
-//エコリス植生図
-var ecoris1 = new ol.layer.Tile({
-    title:"エコリス植生図",
-    origin:"",
-    detail:"",
-	source: new ol.source.XYZ({
-		attributions:[new ol.Attribution({html:"<a href='http://map.ecoris.info/' target='_blank'>エコリス地図タイル</a>"})],
-		url:"http://map.ecoris.info/tiles/vegehill/{z}/{x}/{y}.png",
-		minZoom:5,
-		maxZoom:15
-	})
-});
-var ecoris2 = new ol.layer.Tile({
-    title:"エコリス植生図",
-    origin:"",
-    detail:"",
-	source: new ol.source.XYZ({
-		attributions:[new ol.Attribution({html:"<a href='http://map.ecoris.info/' target='_blank'>エコリス地図タイル</a>"})],
-		url:"http://map.ecoris.info/tiles/vegehill/{z}/{x}/{y}.png",
-		minZoom:5,
-		maxZoom:15
-	})
-});
+//------------------------------------------------------------------------------
+//エクステントの座標系を変換する
+function transformE(extent) {
+	return ol.proj.transformExtent(extent,'EPSG:4326','EPSG:3857');
+};
+//------------------------------------------------------------------------------
+//背景ダイアログ用のテーブルを作成する。haikei.jsで使っている。
+function funcHaikeiTableCreate(mapElement,mapName){
+    if(mapName=="map1"){
+        var layers = useLayersArr1;
+    }else{
+        var layers = useLayersArr2;
+    };
+    var htmlChar = "<div class='haikei-tbl-div'><table class='haikei-tbl table table-bordered table-condensed'>";
+    for(var i = 0; i <layers.length; i++){
+        if(i==0){
+            var chkChar = "checked";
+        }else{
+            var chkChar = "";
+        };
+        if(!Array.isArray(layers[i])){//配列でないとき
+            var prop = layers[i].getProperties();
+        }else{//配列のとき
+            var prop = layers[i][0].getProperties();
+        };
+        htmlChar += "<tr>";
+        htmlChar += "<td><label><input type='checkbox' name='haikei-check' value='" + i + "'" + chkChar + ">　" + prop["title"] + "</label></td>";
+        htmlChar += "<td class='td-slider'><div class='haikei-slider'></div></td>";
+        htmlChar += "<td class='td-sort' title='ドラッグします。'><i class='fa fa-bars fa-lg'></i></td>";
+        htmlChar += "<td class='td-info'><i class='fa fa-info-circle fa-lg primary'></i></td>";
+        htmlChar += "</tr>";
+    };
+    htmlChar += "</table></div>";
+    mapElement.find(".dialog-content").html(htmlChar);
+    funcHaikeiTblDivHeight();//common.jsにある関数
+    mapElement.find(".haikei-slider").eq(0).slider({
+        min:0,max:1,value:1,step:0.01,
+        slide: function(event, ui){
+            layers[0].setOpacity(ui.value);
+        }
+    });
+    mapElement.find(".haikei-tbl tbody").sortable({
+        handle:".td-sort",
+        update:function(event,ui){
+            funcHaikeiLayerSort(mapElement,mapName);
+        }
+    }).disableSelection();
+    //チェックボックスをカスタム
+    mapElement.find("input").iCheck({
+        checkboxClass:"icheckbox_flat-blue",
+        radioClass:"iradio_square-red",
+    });
+    //チェックボックスを押した時★★★★★
+    mapElement.find("input").on("ifChanged",function(event){
+        //背景レイヤーの追加、削除
+        var layer = layers[Number($(this).val())];
+        var trErement = $(this).parents("tr");
+        if($(this).prop("checked")){
+            if(!Array.isArray(layer)){
+                eval(mapName).addLayer(layer);
+                //座標を移動する。
+                if(layer.getProperties()["coord"]){
+                    var lonlat = layer.getProperties()["coord"];
+                    lonlat = ol.proj.transform(lonlat,"EPSG:4326","EPSG:3857");
+                    eval(mapName).getView().setCenter(lonlat);
+                };
+                //ズーム利を設定する。
+                if(layer.getProperties()["zoom"]){
+                    var zoom = layer.getProperties()["zoom"];
+                    console.log(zoom);
+                    eval(mapName).getView().setZoom(zoom);
+                };
+            }else{//配列のとき
+                for(var i = 0; i < layer.length; i++){
+                    eval(mapName).addLayer(layer[i]);
+                };
+            };
+            trErement.children().animate({
+                "background-color":"#FFC0CB"
+            },1000).animate({
+                "background-color":"white"
+            },1000);
+            trErement.prependTo($(this).parents(".haikei-tbl"));
+            funcHaikeiLayerSort(mapElement,mapName);
+        }else{
+            if(!Array.isArray(layer)){
+                eval(mapName).removeLayer(layer);
+            }else{//配列のとき
+                for(var i = 0; i < layer.length; i++){
+                    eval(mapName).removeLayer(layer[i]);
+                };
+            };
+        };
+        var tgtTr = $(this).parents("tr");
+        tgtTr.find(".haikei-slider").slider({
+            min:0,max:1,value:1,step:0.01,
+            slide: function(event, ui){
+                if(!Array.isArray(layer)){
+                    layer.setOpacity(ui.value);
+                }else{
+                    for(var i = 0; i < layer.length; i++){
+                        layer[i].setOpacity(ui.value);
+                    };
+                };
+            }
+        });
+    });
+    $("body").on("click",".td-info",function(){
+        var mapObj = funcMaps($(this));
+        var content = "作成中！！！！！！！！！！！！！！！";
+        mydialog({
+            id:"info-dialog",
+            class:"info-dialog",
+            map:mapObj["name"],
+            title:"インフォメーション",
+            content:content,
+            top:"90px",
+            right:"20px"
+        });
+        return false;
+    });
+};
+//------------------------------------------------------------------------------
+//背景レイヤーの重なり順をｔｒ順に変更する。
+function funcHaikeiLayerSort(mapElement,mapName){
+    mapElement.find(".haikei-tbl tbody tr").each(function(e){
+        if(mapName=="map1"){
+            var layer = useLayersArr1[Number($(this).find("input:checkbox").val())];
+        }else{
+            var layer = useLayersArr2[Number($(this).find("input:checkbox").val())];
+        };
+        if(!Array.isArray(layer)){
+            layer.setZIndex(-e);
+        }else{
+            for (var i = 0; i < layer.length; i++){
+                layer[i].setZIndex(-e);
+            };
+        };
+    });
+};
