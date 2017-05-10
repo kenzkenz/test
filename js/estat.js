a30bac0460ab0bf7894f897a5f7645e414672095
kenzkenzM <kenotiai@gmail.com>
2017-05-10 23:44:06 +0900

    estat

--- js/estat.js
+++ js/estat.js
@@ -1,10 +1,11 @@
+var estatLayermap1 = null;
+var estatLayermap2 = null;
 $(function(){
 	var cityDataAr = null;
 	var citySelectOption = null;
 	var prefSelectOption = null;
 	//----------------------------------------------------------------------------------------------
 	$(".estat-a").click(function(){
-        $.blockUI({message:null});
 		var mapObj = funcMaps($(this));
         var mapName = mapObj["name"];
 		//既に存在しているときは抜ける。
@@ -12,6 +13,7 @@ $(function(){
 			$("#mydialog-estat-dialog-" + mapName).show();
 			return;
 		}
+        $.blockUI({message:null});
         var content = "";
 			content += "<select class='estat-pref-select'></select>";
 			content += "<select class='estat-table-select'></select>";
@@ -35,10 +37,10 @@ $(function(){
         }
 		$("#" + mapName + " .estat-pref-select").html(option);
 		$("#" + mapName + " .estat-pref-select").select2({
-			width:"150px"
+			width:"100px"
 		});
 		$("#" + mapName + " .estat-table-select").select2({
-			width:"200px"
+			width:"250px"
 		});
         //------------------------------------------------------------------------------------------
         var cityTableAjax = function(){
@@ -105,12 +107,23 @@ $(function(){
             $.blockUI({message:null});
             if($(this).val()=="pref"){//全国を選択したとき
 				alert("準備に数秒ほどかかります。");
+                var vectorSource = new ol.source.Vector({
+                    url:"geojson/pref.geojson",
+                    format: new ol.format.GeoJSON()
+                })
+                estatLayerCreate(vectorSource,mapName);
+
+                eval("estatLayer" + mapName).getSource().once("change", function(evt){//
+                    var extent = eval("estatLayer" + mapName).getSource().getExtent();
+                    eval(mapName).getView().fit(extent,eval(mapName).getSize());
+                });
+
                 var tblHtml = "<table class='estat-tbl table table-bordered table-hover tablesorter'>";
                 tblHtml += "<thead><tr class='info'><th>コード</th><th>自治体名</th><th class='estat-zinkou-th'>人口</th><th>value</th><th>単位</th></tr></thead><tbody>";
                 for (var i = 0; i < prefAr.length; i++) {
                     tblHtml += "<tr class='tr-" + prefAr[i]["id"] + "'>";
                     tblHtml += "<td>" + prefAr[i]["id"] + "</td>";
-                    tblHtml += "<td>" + prefAr[i]["name"] + "</td>";
+                    tblHtml += "<td class='estat-city-td'>" + prefAr[i]["name"] + "</td>";
                     tblHtml += "<td class='estat-zinkou-td'>" + "" + "</td>";
                     tblHtml += "<td class='estat-value-td'>" + "" + "</td>";
                     tblHtml += "<td class='estat-unit-td'>" + "" + "</td>";
@@ -123,6 +136,28 @@ $(function(){
                 $.unblockUI();
                 alert("準備終了");
 			}else{//各都道府県を選択したとき
+                var prefName = $("#" + mapName + " .estat-pref-select option:selected").text().split("-")[1];
+                $.ajax({
+                    type:"GET",
+                    url:"php/pref.php",
+                    dataType:"json",
+                    data:{
+                        layerid:"gyouseikai",
+                        prefname:prefName
+                    }
+                }).done(function(json){
+                    console.log(json)
+                    var geojsonObject = json.geojson;
+                    var vectorSource = new ol.source.Vector({
+                        features: (new ol.format.GeoJSON()).readFeatures(geojsonObject,{featureProjection:'EPSG:3857'})
+                    });
+                    estatLayerCreate(vectorSource,mapName);
+                    var extent = eval("estatLayer" + mapName).getSource().getExtent();
+                    eval(mapName).getView().fit(extent,eval(mapName).getSize());
+                    //----------------------------------------------------------
+                }).fail(function(json){
+                    console.log("失敗!");
+                });
                 //国交省のapiを使用　http://www.land.mlit.go.jp/webland/api.html#todofukenlist
                 $("#" + mapName + " .estat-table-select").html(citySelectOption);
                 var tgtUrl = "http://www.land.mlit.go.jp/webland/api/CitySearch?";
@@ -172,7 +207,7 @@ $(function(){
                     for (var i = 0; i < cityAr.length; i++) {
                         tblHtml += "<tr class='tr-" + cityAr[i]["id"] + "'>";
                         tblHtml += "<td>" + cityAr[i]["id"] + "</td>";
-                        tblHtml += "<td>" + cityAr[i]["name"] + "</td>";
+                        tblHtml += "<td class='estat-city-td'>" + cityAr[i]["name"] + "</td>";
                         tblHtml += "<td class='estat-zinkou-td'>" + "" + "</td>";
                         tblHtml += "<td class='estat-value-td'>" + "" + "</td>";
                         tblHtml += "<td class='estat-unit-td'>" + "" + "</td>";
@@ -239,10 +274,10 @@ $(function(){
                 }
                 var selectBox = "<select class='estat-year-select'>" + option + "</select>";
                 if(yearAr.length>0){
-                    $("#" + mapName + " .estat-year-div").html(selectBox + "年　" + $(".estat-table-select option:selected").text().split("-")[1]);
+                    $("#" + mapName + " .estat-year-div").html(selectBox + "年　" + $("#" + mapName + " .estat-table-select option:selected").text().split("-")[1]);
                     var unit = cityDataAr[0]["VALUE"][0]["@unit"];
                 }else{
-                    $("#" + mapName + " .estat-year-div").html(cityDataAr[0]["VALUE"]["@time"] + "年　" + $(".estat-table-select option:selected").text().split("-")[1]);
+                    $("#" + mapName + " .estat-year-div").html(cityDataAr[0]["VALUE"]["@time"] + "年　" + $("#" + mapName + " .estat-table-select option:selected").text().split("-")[1]);
                     var unit = cityDataAr[0]["VALUE"]["@unit"];
                 }
                 $("#" + mapName + " .estat-year-select").select2({
@@ -308,7 +343,6 @@ $(function(){
         $("#" + mapName + " .estat-tbl tbody tr").each(function(){
         	var tgt = ".estat-value-td";
             var value = Number($(this).find(tgt).text());
-            console.log(value);
             if(value>0){//値がプラスだったとき
                 var c100 = (value-min)/color100/100;
                 var color0 = new RGBColor(d3Color(c100));
@@ -320,20 +354,46 @@ $(function(){
                 var rgb = new RGBColor(d3ColorM(c100)).toRGB();
                 var rgba = "rgba(" + color0.r + "," + color0.g + "," + color0.b +"," + "0.8)";
             }
-            console.log(rgba);
-            /*
+            var features = eval("estatLayer" + mapName).getSource().getFeatures();
             for (i=0; i<features.length; i++){
-                if(features[i].getProperties()["市区町村"]==$(this).find(".cityname").text()){
-                    features[i]["C"]["_fillColor"] = rgba;//setPropertiesを使うとエラーがでるのでこっちで
-                    features[i]["C"]["_polygonHeight"] = Math.floor(c100*50000) + 1000;
+                if(features[i].getProperties()["自治体名"]==$(this).find(".estat-city-td").text()){
+                    features[i]["H"]["_fillColor"] = rgba;
+                    features[i]["H"]["_polygonHeight"] = Math.floor(c100*50000) + 1000;
                 }
             }
-            */
+            eval("estatLayer" + mapName).getSource().changed();
             $(this).find("td").css({
                 background:rgba
             });
         });
 	}
+	//----------------------------------------------------------------------------
+    function estatLayerCreate(vectorSource,mapName){
+        eval(mapName).removeLayer(eval("estatLayer" + mapName));
+        this["estatLayer" + mapName] = new ol.layer.Vector({
+            name:"estatLayer_zinkouOn",
+            source:vectorSource,
+            //minResolution:611,
+            style: function(feature,resolution){
+                var fillColor = feature.getProperties()["_fillColor"];
+                style = [
+                    new ol.style.Style({
+                        stroke: new ol.style.Stroke({
+                            color:"gray",
+                            width: 1
+                        }),
+                        fill: new ol.style.Fill({
+                            color:fillColor ? fillColor:"rgba(0,120,200,0.2)"
+                        })
+                    })
+                ];
+                return style;
+            }
+        });
+        eval("estatLayer" + mapName).set("altitudeMode","clampToGround");
+        eval(mapName).addLayer(eval("estatLayer" + mapName));
+        eval("estatLayer" + mapName).setZIndex(9999);
+    }
 	//--------------------------------------------------------------------------
 	function zinkouTdSet(zinkouAr,mapName){
 		for (i=0; i<zinkouAr.length; i++){
