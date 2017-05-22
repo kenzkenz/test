7ea1f0a9bce70318a57390a8029b2ebfb3a2f53e
kenzkenzWS <kenotiai@gmail.com>
2017-05-22 18:29:43 +0900

    トップを表示

--- js/weather.js
+++ js/weather.js
@@ -24,15 +24,25 @@ $(function(){
         });
         var styleFunc = function(feature,resolution){
             var fillColor = feature.getProperties()["_fillColor"];
+            var max = feature.getProperties()["_max"];
+            //console.log(max);
             style = [
                 new ol.style.Style({
                     image:new ol.style.Circle({
-                        radius: 10,
+                        radius: max ? 20:10,
                         fill: new ol.style.Fill({
                             color:fillColor ? fillColor:"white"
                         }),
                         stroke: new ol.style.Stroke({color:"black",width:1})
-                    })
+                    }),
+                    text: new ol.style.Text({
+                        font: "20px helvetica,sans-serif",
+                        text: max ? "max":"",
+                        fill: new ol.style.Fill({
+                            color:"white"
+                        })
+                    }),
+                    zIndex:max ? 2:1
                 })
             ];
             return style;
@@ -67,51 +77,64 @@ $(function(){
             }
         }).done(function (json) {
             var features = eval("weatherLayer" + mapName).getSource().getFeatures();
+            var maxbb = 0;
+            var maxKansokuzyoNo;
             for (i = 0; i < features.length; i++) {
                 features[i]["H"]["_fillColor"] = "rgba(0,0,0,0.0)";
+                features[i]["H"]["_max"] = "";
                 var text = $("#" + mapName + " .weather-select option:selected").text();
                 for (j = 0; j < json["kisyoujson"].length; j++) {
                     if (Number(json["kisyoujson"][j]["観測所番号"]) == features[i]["H"]["観測所番号"]) {
-                        var aa = json["kisyoujson"][j]
+                        var aa = json["kisyoujson"][j];
                         if (pass.split("/")[0] == "pre_rct") {
                             var d3Color = d3.interpolateLab("white", "blue");
-                            var mm = aa["現在値(mm)"];
-                            features[i]["H"]["value"] = {"降水量": mm + "mm"};
-                            var fillColor = d3Color(mm / 10);
+                            var bb = aa["現在値(mm)"];
+                            features[i]["H"]["value"] = {"降水量": bb + "mm"};
+                            var fillColor = d3Color(bb / 10);
                             features[i]["H"]["_fillColor"] = fillColor;
                         }else if (text == "今日の最高気温") {
                             var d3Color = d3.interpolateLab("white", "red");
-                            var temp = aa["今日の最高気温(℃)"];
-                            features[i]["H"]["value"] = {"今日の最高気温(℃)":temp + "℃"};
-                            var fillColor = d3Color(temp / 30);
+                            var bb = aa["今日の最高気温(℃)"];
+                            features[i]["H"]["value"] = {"今日の最高気温(℃)":bb + "℃"};
+                            var fillColor = d3Color((bb/35)*(bb/35));
                             features[i]["H"]["_fillColor"] = fillColor;
                         } else if (text == "今日の最低気温") {
                             var d3Color = d3.interpolateLab("white", "red");
-                            var temp = aa["今日の最低気温(℃)"];
-                            features[i]["H"]["value"] = {"今日の最低気温(℃)":temp + "℃"};
-                            var fillColor = d3Color(temp / 30);
+                            var bb = aa["今日の最低気温(℃)"];
+                            features[i]["H"]["value"] = {"今日の最低気温(℃)":bb + "℃"};
+                            var fillColor = d3Color((bb/25)*(bb/25));
                             features[i]["H"]["_fillColor"] = fillColor;
                         } else if (text == "今日の最大風速") {
                             var d3Color = d3.interpolateLab("white", "green");
-                            var wind = aa["今日の最大値(m/s)"]
-                            features[i]["H"]["value"] = {"今日の最大風速":wind + "(m/s)"};
-                            var fillColor = d3Color(wind / 30);
+                            var bb = aa["今日の最大値(m/s)"];
+                            features[i]["H"]["value"] = {"今日の最大風速":bb + "(m/s)"};
+                            var fillColor = d3Color(bb / 30);
                             features[i]["H"]["_fillColor"] = fillColor;
                         } else if (text == "今日の最大瞬間風速") {
                             var d3Color = d3.interpolateLab("white", "green");
-                            var wind = aa["今日の最大値(m/s)"]
-                            features[i]["H"]["value"] = {"今日の最大瞬間風速":wind + "(m/s)"};
-                            var fillColor = d3Color(wind / 30);
+                            var bb = aa["今日の最大値(m/s)"];
+                            features[i]["H"]["value"] = {"今日の最大瞬間風速":bb + "(m/s)"};
+                            var fillColor = d3Color(bb / 30);
                             features[i]["H"]["_fillColor"] = fillColor;
                         }
                         json["kisyoujson"].splice(j, 1);
+                        if(maxbb<Number(bb)){
+                            maxbb = Number(bb);
+                            maxKansokuzyoNo = features[i]["H"]["観測所番号"];
+                        }
                         break;
                     }
                 }
             }
+            //console.log(maxbb);
+            //console.log(maxKansokuzyoNo);
+            for (i = 0; i < features.length; i++) {
+                if (maxKansokuzyoNo == features[i]["H"]["観測所番号"]) {
+                    features[i]["H"]["_max"] = 1;
+                    break
+                }
+            }
             eval("weatherLayer" + mapName).getSource().changed();
-           // eval(mapName).removeLayer(eval("weatherLayer" + mapName));
-           // eval(mapName).addLayer(eval("weatherLayer" + mapName));
         });
     });
 });
