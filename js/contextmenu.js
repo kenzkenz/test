b48ea7ae97a5a003552bb8a457eae6872cc96cf8
kenzkenzWS <kenotiai@gmail.com>
2017-05-23 17:55:32 +0900

    右クリック実験中

--- js/contextmenu.js
+++ js/contextmenu.js
@@ -1,21 +1,26 @@
+var myContextOverlay = null;
 $(function(){
 
     //自作コンテキストメニュー（右クリックメニュー）
-    var myContextOverlay = new ol.Overlay({
+    myContextOverlay = new ol.Overlay({
         element:$(".myContextOverlay-div")[0]
     });
     map1.addOverlay(myContextOverlay);
 
-
     $("#map1")[0].addEventListener('contextmenu',myContextmenu,false);
     $("body").on("mouseenter",".dialog-content,.dialog-base",function(){//contentにマウスが当たったら通常の右クリックメニュー復活。
         $("#map1")[0].removeEventListener('contextmenu',myContextmenu,false);
+        $(".myContextOverlay-div").show();
     }).on("mouseleave",".dialog-content,.dialog-base",function(){//contentからマウスが抜けたら通常の右クリックメニューを無効化。
         $("#map1")[0].addEventListener('contextmenu',myContextmenu,false);
     })
 });
 
-function myContextmenu(e){
-    console.log(111);
-    e.preventDefault();
+function myContextmenu(evt){
+    var myContextmenuTop = evt.clientY;
+    var myContextmenuLeft = evt.clientX;
+    var coord = map1.getCoordinateFromPixel([myContextmenuLeft,myContextmenuTop]);
+    console.log(coord);
+    evt.preventDefault();
+    myContextOverlay.setPosition(coord);
 }
